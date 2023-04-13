9abffcf49b697710503c369beb9b301a4f1c2337
Tazmaniego <DiegoAbrahamMontiel@gmail.com>
2023-04-12 19:07:03 -0700

    webdriverio

--- test/specs/dropdown_list.spec.js
+++ test/specs/dropdown_list.spec.js
@@ -2,20 +2,20 @@ import DropdownListPage from "../pageobjects/dropdown_list.page.js";
 
 describe('My DropdownList Page', () => {
     it('should select input', async () => {
-        await DropdownListPage.open()
-        await browser.pause(3000)
-        await (DropdownListPage).DropList.click()
-        await browser.pause(1500)
-        await (DropdownListPage).SelectList1.click()
-        await browser.pause(1500)
-        await (DropdownListPage).DropList.click()
-        await browser.pause(1500)
-        await (DropdownListPage).DropList.click()
-        await browser.pause(1500)
-        await (DropdownListPage).SelectList2.click()
-        await browser.pause(1500)
-        await (DropdownListPage).DropList.click()
-        await browser.pause(3000)
+        await DropdownListPage.open();
+        await browser.pause(3000);
+        await (DropdownListPage).DropList.click();
+        await browser.pause(1500);
+        await (DropdownListPage).SelectList1.click();
+        await browser.pause(1500);
+        await (DropdownListPage).DropList.click();
+        await browser.pause(1500);
+        await (DropdownListPage).DropList.click();
+        await browser.pause(1500);
+        await (DropdownListPage).SelectList2.click();
+        await browser.pause(1500);
+        await (DropdownListPage).DropList.click();
+        await browser.pause(3000);
         
         // await expect(SecurePage.flashAlert).toHaveTextContaining(
         //     'You logged into a secure area!')
